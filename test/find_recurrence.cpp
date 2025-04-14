ae2e8cf1c99ce7cf67abc35b9d4a6a7be447e465
Yongyi Chen <yongyi781@outlook.com>
2025-04-14 01:57:05 -0400

    find_recurrence test fix

--- test/find_recurrence.cpp
+++ test/find_recurrence.cpp
@@ -9,11 +9,11 @@ using namespace std;
 int main()
 {
     vector<int64_t> const v{3, 10, 23, 44, 77, 128, 205, 320, 491, 744, 1117, 1666, 2473, 3658, 5397};
-    vector<int64_t> const expected{3, -3, 2, -2, 1};
+    vector<int64_t> const expected{1, -2, 2, -3, 3, -1};
     assertEqual(findRecurrence(v, (int64_t)1'000'000'007), expected);
 
     vector<boost::rational<int64_t>> v2{3, 10, 23, 44, 77, 128, 205, 320, 491, 744, 1117, 1666, 2473, 3658, 5397};
-    vector<boost::rational<int64_t>> const expected2{3, -3, 2, -2, 1};
+    vector<boost::rational<int64_t>> const expected2{1, -2, 2, -3, 3, -1};
     assertEqual(findRecurrence(v2), expected2);
     pass("find_recurrence");
 }
