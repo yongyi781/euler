5a097de253b84bc9c7e521a9f22a5cf99fb89084
Yongyi Chen <yongyi781@outlook.com>
2025-04-16 16:54:27 -0400

    Simplified QD algorithm

--- include/euler/QD.hpp
+++ include/euler/QD.hpp
@@ -29,11 +29,10 @@ struct QD
         template <typename Fun> it::result_t operator()(Fun f) const
         {
             using DT = double_integer_t<T>;
-            // Idea: each remainder is of the form (x + c√b) / d, starting with x = a*c and d = c².
+            // Idea: each remainder is of the form (x + c√b) / d, starting with x = a*c and d = c². k stores the floor.
             T const fl = isqrt(c * c * b) * (c > 0 ? 1 : -1);
             DT x = a * c;
             T d = c * c;
-            // k = floor.
             T k = floorDiv(a * c + fl, c * c);
             if (!it::callbackResult(f, k))
                 return it::result_break;
@@ -77,8 +76,9 @@ struct QD
         friend std::basic_ostream<CharT, Traits> &operator<<(std::basic_ostream<CharT, Traits> &o,
                                                              const affine_transform &a)
         {
-            return o << '(' << a.p << "x + " << a.q << "y + " << a.k << ", " << a.r << "x + " << a.s << "y + " << a.l
-                     << ')';
+            std::ostringstream oss;
+            oss << '(' << a.p << "x + " << a.q << "y + " << a.k << ", " << a.r << "x + " << a.s << "y + " << a.l << ')';
+            return o << std::move(oss).str();
         }
     };
 
@@ -111,12 +111,18 @@ struct QD
         i64 const D = disc();
         assert(D > 0 && !isSquare(D) && "Non-hyperbolic QDs not implemented");
 
-        if (d == 0 && e == 0 && std::gcd(a, f) == 1)
-            return solve_af_coprime<T>(0, 0, 1, std::array<i64, 4>{1, 0, 0, 1});
-
         auto const alpha = 2 * c * d - b * e;
         auto const beta = 2 * a * e - b * d;
-        i64 const n = -D * (a * e * e - b * e * d + c * d * d + f * D);
+        i64 n = -f;
+        i64 denom = 1;
+        if (d != 0 || e != 0)
+        {
+            n = -D * (a * e * e - b * e * d + c * d * d + f * D);
+            denom = D;
+        }
+        else if (std::gcd(a, f) == 1)
+            return solve_af_coprime<T>(0, 0, 1, std::array<i64, 4>{1, 0, 0, 1});
+
         if (n == 0)
             return {Vector<T, 2>{}};
 
@@ -149,21 +155,23 @@ struct QD
                 }
             }
         }
-        return q.solve_af_coprime<T>(alpha, beta, D, coeffs);
+        return q.solve_af_coprime<T>(alpha, beta, denom, coeffs);
     }
 
-    /// Returns all integral solutions to `ax^2 + bxy + cy^2 + dx + ey + f == 0` within the specified range.
-    template <integral2 T> [[nodiscard]] std::vector<Vector<T, 2>> solve(T xmin, T xmax, T ymin, T ymax)
+    /// Returns all integral solutions to `ax^2 + bxy + cy^2 + dx + ey + f == 0` within the specified range, sorted.
+    template <integral2 T1, integral2 T2, integral2 T3, integral2 T4>
+    [[nodiscard]] auto solve(T1 xmin, T2 xmax, T3 ymin, T4 ymax)
     {
+        using T = std::common_type_t<T1, T2, T3, T4>;
         using std::abs;
 
-        auto sols = solve();
+        auto sols = solve<T>();
         auto const rec = recurrence();
         if (sols.size() == 1 && sols[0][0] == 0 && sols[0][1] == 0 && rec.k == 0 && rec.l == 0)
-            return {{}};
+            return std::vector<Vector<T, 2>>{{}};
         auto const inv = rec.inverse();
-        T const xlimit = std::max({abs(xmin), abs(xmax)});
-        T const ylimit = std::max({abs(ymin), abs(ymax)});
+        T const xlimit = std::max({T(abs(xmin)), T(abs(xmax))});
+        T const ylimit = std::max({T(abs(ymin)), T(abs(ymax))});
         std::vector<Vector<T, 2>> res;
         for (auto &&v0 : sols)
         {
@@ -183,12 +191,12 @@ struct QD
                 v = inv(v);
             }
         }
-
         std::ranges::sort(res);
-        auto const u = std::ranges::unique(res);
-        res.erase(u.begin(), u.end());
         return res;
     }
+
+    /// Returns all integral solutions to `ax^2 + bxy + cy^2 + dx + ey + f == 0` within the specified range, sorted.
+    template <integral2 T1, integral2 T2> [[nodiscard]] auto solve(T1 min, T2 max) { return solve(min, max, min, max); }
 
     /// Gets the forward recurrence for solutions to this quadratic diophantine equation.
     template <integral2 T = i64> [[nodiscard]] affine_transform recurrence() const
@@ -226,28 +234,17 @@ struct QD
   private:
     /// Primitive undamental solutions to `ax^2 + bxy + cy^2 + f == 0`, subject to a predicate.
     /// Precondition: `gcd(a, b, c) = 1` and `gcd(a, f) = 1`.
-    template <integral2 T = i64, typename Pred>
-    [[nodiscard]] std::vector<Vector<T, 2>> solvePrimitive_af_coprime(Pred pred) const
+    template <integral2 T = i64, typename Callback> void solvePrimitive_af_coprime(Callback visit) const
     {
-        std::vector<Vector<T, 2>> res;
         i64 const n = -f;
-        auto const proc = [&](i64 t, i64 p, i64 q, i64 r, const T &k, const T &y) -> bool {
-            bool found = false;
+        auto const proc = [&](i64 t, i64 p, i64 q, i64 r, const T &k, const T &y) -> it::result_t {
             if (p * y * y + q * y * k + r * k * k == 1)
             {
                 T const x = t * y - n * k;
-                if (pred(x, y))
-                {
-                    found = true;
-                    res.emplace_back(x, y);
-                }
-                if (pred(-x, -y))
-                {
-                    found = true;
-                    res.emplace_back(-x, -y);
-                }
+                bool found_pos = visit(x, y), found_neg = visit(-x, -y);
+                return found_pos || found_neg ? it::result_break : it::result_continue;
             }
-            return !found;
+            return it::result_continue;
         };
         for (i64 t : Quadratic(a, b, c).solveMod(n))
         {
@@ -259,34 +256,24 @@ struct QD
             if (r == 1)
             {
                 // We have 1/0 as a solution.
-                bool found = false;
-                if (pred(1, t))
-                {
-                    found = true;
-                    res.emplace_back(1, t);
-                }
-                if (pred(-1, -t))
-                {
-                    found = true;
-                    res.emplace_back(-1, -t);
-                }
-                if (found)
+                bool found_pos = visit(T(1), T(t)), found_neg = visit(T(-1), T(-t));
+                if (found_pos || found_neg)
                     continue;
             }
             if (gcd({p, q, r}) != 1)
                 continue;
 
-            cfrac_quadratic_one_period<T>(-q, disc(), 2 * r).template convergents<T>()([&](auto &&ky) {
-                return proc(t, p, q, r, ky.first, ky.second);
-            });
-            cfrac_quadratic_one_period<T>(q, disc(), -2 * r).template convergents<T>()([&](auto &&ky) {
-                return proc(t, p, q, r, ky.first, ky.second);
-            });
+            if (cfrac_quadratic_one_period<T>(q, disc(), -2 * r).template convergents<T>()([&](auto &&ky) {
+                    return proc(t, p, q, r, ky.first, ky.second);
+                }) == it::result_continue)
+                // Return value of result_continue means solution not found, need to check other root of quadratic.
+                cfrac_quadratic_one_period<T>(-q, disc(), 2 * r).template convergents<T>()([&](auto &&ky) {
+                    return proc(t, p, q, r, ky.first, ky.second);
+                });
         }
-        return res;
     }
 
-    /// Fundamental solutions to ax^2 + bxy + cy^2 + f == 0 and gcd(x, y) = 1.
+    /// Returns a set of fundamental solutions to ax^2 + bxy + cy^2 + f == 0 and gcd(x, y) = 1.
     /// Precondition: gcd(a, b, c) = 1 and gcd(a, f) = 1.
     /// Here, D is not the discriminant but rather the denominator of the Legendre transformation.
     template <integral2 T = i64>
@@ -299,18 +286,15 @@ struct QD
         for (auto const g : divisors(sqfreeDecompose(-f).first))
         {
             q.f = f / (g * g);
-            for (auto const [u, v] : q.solvePrimitive_af_coprime<T>([&](auto &&u, auto &&v) {
-                     return (g * (coeffs[0] * u + coeffs[1] * v) + alpha) % D == 0 &&
-                            (g * (coeffs[2] * u + coeffs[3] * v) + beta) % D == 0;
-                 }))
-            {
-                res.emplace_back((g * (coeffs[0] * u + coeffs[1] * v) + alpha) / D,
-                                 (g * (coeffs[2] * u + coeffs[3] * v) + beta) / D);
-            }
+            q.solvePrimitive_af_coprime<T>([&](const T &u, const T &v) {
+                T const xD = g * (coeffs[0] * u + coeffs[1] * v) + alpha;
+                T const yD = g * (coeffs[2] * u + coeffs[3] * v) + beta;
+                if (xD % D != 0 || yD % D != 0)
+                    return false;
+                res.emplace_back(xD / D, yD / D);
+                return true;
+            });
         }
-        std::ranges::sort(res);
-        auto const u = std::ranges::unique(res);
-        res.erase(u.begin(), u.end());
         return res;
     }
 };
