c03cd2e672c2f4f6d1b9b9935b269a1a9a2f14ae
Yongyi <yongyi781@outlook.com>
2025-12-05 23:00:56 -0500

    Added dynamic prime FactorialMod and improved Quadratic p=2 handling, and removed gcd(a,b,c)=1 precondition

--- include/euler/FactorialMod.hpp
+++ include/euler/FactorialMod.hpp
@@ -84,4 +84,82 @@ template <integral2 auto M, bool ComputeInverse = true> class FactorialMod
 
     void resize(size_t newSize) { resize(std::execution::par, newSize); }
 };
+
+/// Class to compute factorials and binomial coefficients modulo a dynamic modulus.
+template <integral2 T, bool ComputeInverse = true> class FactorialModX
+{
+    T p_{};
+    std::vector<T> fact;
+    std::vector<T> invFact;
+
+    template <execution_policy Exec> void computeFactorials(Exec &&exec)
+    {
+        fact[0] = 1;
+        std::inclusive_scan(std::forward<Exec>(exec), counting_iterator(T(1)), counting_iterator(T(fact.size())),
+                            std::next(fact.begin()), mod_multiplies{p_});
+    }
+
+    template <execution_policy Exec> void computeInverseFactorials(Exec &&exec)
+    {
+        std::transform_exclusive_scan(
+            std::forward<Exec>(exec), counting_iterator(T(0)), counting_iterator(T(invFact.size())), invFact.rbegin(),
+            modInverse(fact.back(), p_), mod_multiplies{p_}, [&](T x) -> T { return invFact.size() - 1 - x; });
+    }
+
+  public:
+    using value_type = T;
+
+    template <execution_policy Exec>
+        requires(!std::is_integral_v<Exec>)
+    FactorialModX(Exec &&exec, T p, size_t limit) : p_(p), fact(limit + 1, 0), invFact(limit + 1, 0)
+    {
+        if (limit > 0)
+        {
+            computeFactorials(std::forward<Exec>(exec));
+            if constexpr (ComputeInverse)
+                computeInverseFactorials(std::forward<Exec>(exec));
+        }
+    }
+
+    explicit FactorialModX(T p, size_t limit) : FactorialModX(std::execution::seq, p, limit) {}
+
+    [[nodiscard]] value_type operator()(int64_t n) const { return factorial(n); }
+
+    [[nodiscard]] value_type factorial(int64_t n) const
+    {
+        assert(n >= 0 && n <= (int64_t)fact.size());
+        return fact[n];
+    }
+
+    [[nodiscard]] value_type inverseFactorial(int64_t n) const
+    {
+        assert(n >= 0 && n <= (int64_t)fact.size());
+        if constexpr (ComputeInverse)
+            return invFact[n];
+        else
+            return modInverse(fact[n], p_);
+    }
+
+    [[nodiscard]] value_type binomial(int64_t n, int64_t r) const
+    {
+        assert(n >= 0 && n <= (int64_t)fact.size());
+        if (r < 0 || r > n)
+            return (int64_t)0;
+        return factorial(n) * inverseFactorial(n - r) % p_ * inverseFactorial(r) % p_;
+    }
+
+    template <execution_policy Exec> void resize(Exec &&exec, size_t limit)
+    {
+        if (limit > fact.size())
+        {
+            fact.resize(limit + 1);
+            invFact.resize(limit + 1);
+            computeFactorials(std::forward<Exec>(exec));
+            if constexpr (ComputeInverse)
+                computeInverseFactorials(std::forward<Exec>(exec));
+        }
+    }
+
+    void resize(size_t newSize) { resize(std::execution::par, newSize); }
+};
 } // namespace euler
--- include/euler/Quadratic.hpp
+++ include/euler/Quadratic.hpp
@@ -11,14 +11,23 @@
 
 namespace euler
 {
-/// Quadratic equation, with methods to solve mod primes and powers of primes. Precondition: gcd(a, b, c) = 1.
+/// Quadratic equation, with methods to solve mod primes and powers of primes.
 struct Quadratic
 {
     i64 a, b, c;
 
-    /// Solves ax^2 + bx + c == 0 (mod p). Precondition: gcd(a, b, c) = 1.
+    /// Solves ax^2 + bx + c == 0 (mod p).
     [[nodiscard]] constexpr std::vector<i64> solveModPrime(i64 p) const
     {
+        if (p == 2)
+        {
+            std::vector<i64> res;
+            if (c % 2 == 0)
+                res.push_back(0);
+            if ((a + b + c) % 2 == 0)
+                res.push_back(1);
+            return res;
+        }
         auto const a = mod(this->a, p);
         auto const b = mod(this->b, p);
         auto const c = mod(this->c, p);
@@ -26,7 +35,7 @@ struct Quadratic
         if (a == 0)
         {
             if (b == 0)
-                return {};
+                return c == 0 ? range(0, p - 1) : std::vector<i64>{};
             return {mod(-c * modInverse(b, p), p)};
         }
         auto const d = mod(b * b - 4 * a * c, p);
@@ -42,45 +51,42 @@ struct Quadratic
         return {x2, x1};
     }
 
-    /// Solves ax^2 + bx + c == 0 (mod p^e). Precondition: gcd(a, b, c) = 1.
+    /// Solves ax^2 + bx + c == 0 (mod p^e).
     [[nodiscard]] constexpr std::vector<i64> solveModPrimePower(i64 p, int e) const
     {
-        if (p == 2)
+        auto sols = solveModPrime(p);
+        if (sols.empty())
+            return {};
+        std::vector<i64> next_sols;
+        i64 q = p;
+        for (; --e > 0; q *= p)
         {
-            // Brute force (TODO: do this properly at some point).
-            i64 const q = 1_i64 << e;
-            return it::range(0, q - 1).filter([&](i64 x) { return (a * x * x + b * x + c) % q == 0; }).to();
-        }
-        std::vector<std::pair<i64, int>> v = mapv(solveModPrime(p), [](i64 x) { return std::pair{x, 1}; });
-        std::vector<i64> res;
-        while (!v.empty())
-        {
-            auto const [x, i] = v.back();
-            v.pop_back();
-            if (i == e)
-                res.push_back(x);
-            else
+            // Hensel lifting.
+            next_sols.clear();
+            auto const q2 = q * p;
+            for (auto &&x : sols)
             {
-                // Hensel lifting.
-                auto const q = pow(p, i);
-                auto const num = mod(a * x * x + b * x + c, p * q);
+                auto const num = mod(a * x % q2 * x % q2 + b * x + c, q2);
                 auto const den = 2 * a * x + b;
                 if (den % p != 0)
-                    v.emplace_back(mod(x - num * modInverse(den, p * q), p * q), i + 1);
+                    next_sols.push_back(mod(x - num * modInverse(den, q2), q2));
                 else if (num == 0)
                     for (i64 k = 0; k < p; ++k)
-                        v.emplace_back(x + k * q, i + 1);
+                        next_sols.push_back(x + k * q);
             }
+            if (next_sols.empty())
+                return {};
+            swap(sols, next_sols);
         }
-        std::ranges::sort(res);
-        return res;
+        std::ranges::sort(sols);
+        return sols;
     }
 
-    /// Solves ax^2 + bx + c == 0 (mod n). Precondition: gcd(a, b, c) = 1.
+    /// Solves ax^2 + bx + c == 0 (mod n).
     [[nodiscard]] constexpr std::vector<i64> solveMod(i64 n) const
     {
         std::vector<i64> res{0};
-        std::vector<i64> copy;
+        std::vector<i64> next_res;
         i64 m = 1;
         it::factor{n}([&](auto &&pe) {
             auto &&[p, e] = pe;
@@ -91,12 +97,12 @@ struct Quadratic
                 res.clear();
                 return it::result_break;
             }
-            swap(copy, res);
-            res.clear();
-            for (auto x : copy)
+            next_res.clear();
+            for (auto x : res)
                 for (auto y : sols)
-                    res.push_back(crt(x, y, m, q));
+                    next_res.push_back(crt(x, y, m, q));
             m *= q;
+            swap(res, next_res);
             return it::result_continue;
         });
         std::ranges::sort(res);
